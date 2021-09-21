b1c3e42d9a0aacf7a8f91b6b418bdc6db29e3eba
angel <angel@ANGEL-PC>
2021-09-21 16:45:35 +0200

    Agregar archivos de proyecto.

---
+++ LINQ_Kata.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Linq;
+
+public class LINQ_Kata {
+    public static int Sum(int[] integers) {
+        // Sum for Nullable Numeric Types int? sum = numbers.Sum()
+        int ret = integers.Sum();
+        return ret;
+    }
+
+    public static int CountChar(char[] chars, char charToCount) {
+        // (from x in items select x).Count();
+        var ret = (from x in chars select x).Count();
+        return ret;
+    }
+
+    public static int[] CalculateSquares(int start, int end) {
+        //IEnumerable<int> squares = Enumerable.Range(1, 10).Select(x => x * x);
+        var list = Enumerable.Range(start, end);
+        var ret = list.Select(i => i * i);
+        return ret.ToArray();
+    }
+
+    public static int SquareDigits(int n) {
+        //???
+        string digits = n.ToString();
+        string result = "";
+
+        foreach (char character in digits) {
+            int number = Convert.ToInt32(character.ToString());
+            int square = number * number;
+            result += square;
+        }
+        return int.Parse(result);
+    }
+
+    public static string Replace(string s) {
+        var chars = s.Select(ReplaceVocals).ToArray();
+        string ret = new string(chars);
+        return ret;
+    }
+
+    private static char ReplaceVocals(char c) {
+        // Aqui Edu usa operador ternario
+        if ("aeiouAEIOU".Contains(c)) {
+            return '!';
+        }
+        return c;
+    }
+}
---
+++ LINQ_KataTest.cs
@@ -0,0 +1,68 @@
+﻿using NUnit.Framework;
+using System.IO;
+
+[TestFixture]
+public class LINQ_KataTest
+{
+	[Test]
+	public void SumOfArrayTest()
+	{
+		Assert.AreEqual(6, LINQ_Kata.Sum(new[] { 2, 3, 1 }));
+	}
+
+	[Test]
+	public void CountOccurencesOfCharacterTest()
+	{
+		Assert.AreEqual(2, LINQ_Kata.CountChar(new[] { 'A', 'B', 'C', 'A' }, 'A'));
+	}
+
+	[Test]
+	public void CalculateSquaresTest()
+	{
+		Assert.AreEqual(new[] { 1, 4, 9, 16, 25, 36 }, LINQ_Kata.CalculateSquares(1, 6));
+	}
+	[Test]
+	public void ReplaceTest()
+	{
+		Assert.AreEqual("H!!", LINQ_Kata.Replace("Hi!"));
+		Assert.AreEqual("!H!! H!!", LINQ_Kata.Replace("!Hi! Hi!"));
+		Assert.AreEqual("!!!!!", LINQ_Kata.Replace("aeiou"));
+		Assert.AreEqual("!BCD!", LINQ_Kata.Replace("ABCDE"));
+	}	
+	[Test]
+	public void SquareDigitsTest()
+	{
+		Assert.AreEqual(811181, LINQ_Kata.SquareDigits(9119));
+		Assert.AreEqual(0, LINQ_Kata.SquareDigits(0));
+	}
+
+	[TestCase("for")]
+	[TestCase("while")]
+	public void FindWord(string input)
+	{
+		//the path of the file
+		FileStream inFile = new FileStream(@"..\..\LINQ_Kata.cs", FileMode.Open, FileAccess.Read);
+		StreamReader reader = new StreamReader(inFile);
+		string record;
+		try
+		{
+			//the program reads the record and displays it on the screen
+			record = reader.ReadLine();
+			while (record != null)
+			{
+				if (record.Contains(input))
+				{
+					Assert.Fail(input + " found");
+				}
+				record = reader.ReadLine();
+			}
+			Assert.Pass("");
+		}
+		finally
+		{
+			//after the record is done being read, the progam closes
+			reader.Close();
+			inFile.Close();
+		}
+	}
+}
